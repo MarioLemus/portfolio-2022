5e010303e87d449cf0ef17b342c65bdf5c80dec7
MarioLemus <theliarnorland@gmail.com>
2022-05-20 13:26:25 -0600

    styles

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const socialMediaContent = `
 
 <p class='hero-contact' >Contactame</p>
 
-<a href='/' title='LinkedIn' class='hero-icon'>
+<a href='https://www.linkedin.com/in/mariolemus021dev/' target="_blank" title='LinkedIn' class='hero-icon'>
 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler  icon-tabler-brand-linkedin" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <rect x="4" y="4" width="16" height="16" rx="2" />
@@ -34,7 +34,7 @@ const socialMediaContent = `
 </a>
 
 
-<a href='/' title='Github' class='hero-icon'>
+<a href='https://github.com/MarioLemus' target="_blank" title='Github' class='hero-icon'>
 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
